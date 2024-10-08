b64e0c11321346f65bc4a5a860f9642f1293b63c
GraffN <jorgen.graff-n@outlook.com>
2024-10-08 16:08:13 +0200

    new test

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/main/LibraryApplication.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/main/LibraryApplication.java
@@ -18,7 +18,7 @@ public class LibraryApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
-		//test change
+		//test change 2
 	}
 
 }
